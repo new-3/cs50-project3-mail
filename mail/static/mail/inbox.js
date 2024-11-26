b68b546b10b9e5e37e195bc46d0367ff36aaa738
= <=>
2024-11-27 00:24:29 +0900

    FEAT: load mailbox, load mail - render inbox, sent box - clicking the subject of mails displays each detailed mail. - reading mail marks each mail as 'read'

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     event.preventDefault();
     send_email();
     return false;
-    });
+  });
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -21,6 +21,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -30,17 +31,53 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Show header of mail list
+  const headerDiv = document.createElement('div')
+  headerDiv.className = 'row my-1 px-1';
+  ['Sender', 'Subject', 'Timestamp'].forEach(divName => {
+    let headerCol = document.createElement('div');
+    headerCol.className = 'col mx-1';
+    headerCol.innerHTML = `<strong>${divName}</strong>`;
+    headerDiv.appendChild(headerCol);
+  })
+  document.querySelector('#emails-view').append(headerDiv);
+
+  // Send GET request to get mailbox data
+  fetch(`/emails/${mailbox}`, { method: 'GET' })
+    .then(response => response.json())
+    .then(result => {
+      result.forEach(item => {
+        // Create Div for display Each Mail's sender, subject, timestamp
+        const emailList = document.createElement('div');
+        emailList.id = 'mail';
+        emailList.className = 'border row my-1 px-1';
+        ['sender', 'subject', 'timestamp'].forEach(div => {
+          let childDiv = document.createElement('div');
+          childDiv.id = div;
+          childDiv.className = 'col mx-1';
+          childDiv.innerHTML = `<p>${item[div]}</p>`;
+          if (div === 'subject') {
+            childDiv.addEventListener('click', () => load_email(item['id']))
+            childDiv.className += ' font-weight-bold'
+          }
+          emailList.appendChild(childDiv);
+        });
+        if (item['read']) { emailList.style = 'background: lightgray'; }
+        document.querySelector('#emails-view').append(emailList);
+      })
+    });
 }
 
 function send_email() {
-  // parse with comma
   const recipients = document.querySelector('#compose-recipients').value;
   const subject = document.querySelector('#compose-subject').value;
   const body = document.querySelector('#compose-body').value;
@@ -54,9 +91,51 @@ function send_email() {
       body: body
     })
   })
-  .then(response => response.json())
-  .then(result => {
-    console.log(result);
-    load_mailbox('sent');
-});
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      load_mailbox('sent');
+    });
+}
+
+function load_email(id) {
+
+  // Show the mailbox and hide other views
+  document.querySelector('#email-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Reset the email view 
+  document.querySelector('#email-view').innerHTML = "";
+
+  // call API and render email
+  fetch(`/emails/${id}`)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      const mailBody = document.querySelector('#email-view').appendChild(document.createElement('div'));
+      mailBody.id = 'mailBody';
+      mailBody.className = "my-1 px-1";
+      ['sender', 'recipients', 'subject', 'timestamp', 'body'].forEach(id => {
+        childDiv = document.createElement('div');
+        childDiv.id = id;
+        childDiv.className = "my-1 py-1";
+        const header = document.createElement('h6');
+        const content = document.createElement('p');
+        header.innerHTML = `${id.charAt(0).toUpperCase() + id.slice(1)}`
+        content.innerHTML = `${result[id]}`
+        childDiv.appendChild(header);
+        childDiv.appendChild(content);
+        mailBody.appendChild(childDiv);
+      });
+
+      // Mark the email as read (Send PUT request)
+      fetch(`/emails/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          read: true
+        })
+      })
+    }
+    );
 }
